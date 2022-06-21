39627890085180fd113d7aa8df68edf7a4322ca5
sublatelog <blueganz@gmail.com>
2022-06-21 15:55:51 +0900

    Update model.py

--- FirstPhase/model.py
+++ FirstPhase/model.py
@@ -81,11 +81,14 @@ def train(args, train_dataset, model, tokenizer):
             if global_step % save_steps == 0:
                 if args.evaluate_during_training:
                     results = evaluate(args, model, tokenizer)
+                    
                 print('loss: ' + str((tr_loss - logging_loss)/save_steps) + ' step: ' + str(global_step))
                 logging_loss = tr_loss
                 output_dir = os.path.join(args.output_dir, 'checkpoint-{}'.format(global_step))
+                
                 if not os.path.exists(output_dir):
                     os.makedirs(output_dir)
+                    
                 model_to_save = model.module if hasattr(model, 'module') else model
                 model_to_save.save_pretrained(output_dir)
                 torch.save(args, os.path.join(output_dir, 'training_args.bin'))
@@ -325,54 +328,27 @@ class PairProcessor(DataProcessor):
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--data_dir", default=None, 
-                        type=str, required=True,
-                        help="The input data dir. Should contain the .tsv "
-                        "files (or other data files) for the task.")
-    parser.add_argument("--output_dir", default='run_glue_test', 
-                        type=str, required=True,
-                        help="The output directory where the model "
-                        "predictions and checkpoints will be written.")
-    parser.add_argument("--window_size", type=int, required=True,
-                        help="The window size.")                    
-    parser.add_argument("--max_seq_length", default=105, type=int,
-                        help="The maximum total input sequence length "
-                        "after tokenization. Sequences longer than this "
-                        "will be truncated, sequences shorter will be padded.")
-    parser.add_argument("--do_train", action='store_true',
-                        help="Whether to run training.")
-    parser.add_argument("--do_test", action='store_true',
-                        help="Whether to run test set.")
-    parser.add_argument("--do_eval", action='store_true',
-                        help="Whether to run eval on the dev set.")
-    parser.add_argument("--evaluate_during_training", action='store_true',
-                        help="Rul evaluation during training at each logging step.")
-    parser.add_argument("--do_lower_case", action='store_true',
-                        help="Set this flag if you are using an uncased model.")
-    parser.add_argument("--per_gpu_train_batch_size", default=8, type=int,
-                        help="Batch size per GPU/CPU for training.")
-    parser.add_argument("--per_gpu_eval_batch_size", default=8, type=int,
-                        help="Batch size per GPU/CPU for evaluation.")
-    parser.add_argument("--learning_rate", default=5e-5, type=float,
-                        help="The initial learning rate for Adam.")
-    parser.add_argument("--weight_decay", default=0.0, type=float,
-                        help="Weight deay if we apply some.")
-    parser.add_argument("--adam_epsilon", default=1e-8, type=float,
-                        help="Epsilon for Adam optimizer.")
-    parser.add_argument("--max_grad_norm", default=1.0, type=float,
-                        help="Max gradient norm.")
-    parser.add_argument("--num_train_epochs", default=3.0, type=float,
-                        help="Total number of training epochs to perform.")
-    parser.add_argument("--warmup_steps", default=0, type=int,
-                        help="Linear warmup over warmup_steps.")
-    parser.add_argument("--no_cuda", action='store_true',
-                        help="Avoid using CUDA when available")
-    parser.add_argument('--overwrite_output_dir', action='store_true',
-                        help="Overwrite the content of the output directory")
-    parser.add_argument('--overwrite_cache', action='store_true',
-                        help="Overwrite the cached training and evaluation sets")
-    parser.add_argument('--seed', type=int, default=42,
-                        help="random seed for initialization")
+    parser.add_argument("--data_dir", default=None, type=str, required=True, help="The input data dir. Should contain the .tsv files (or other data files) for the task.")
+    parser.add_argument("--output_dir", default='run_glue_test', type=str, required=True, help="The output directory where the model predictions and checkpoints will be written.")
+    parser.add_argument("--window_size", type=int, required=True, help="The window size.")                    
+    parser.add_argument("--max_seq_length", default=105, type=int, help="The maximum total input sequence length after tokenization. Sequences longer than this will be truncated, sequences shorter will be padded.")
+    parser.add_argument("--do_train", action='store_true', help="Whether to run training.")
+    parser.add_argument("--do_test", action='store_true', help="Whether to run test set.")
+    parser.add_argument("--do_eval", action='store_true', help="Whether to run eval on the dev set.")
+    parser.add_argument("--evaluate_during_training", action='store_true', help="Rul evaluation during training at each logging step.")
+    parser.add_argument("--do_lower_case", action='store_true', help="Set this flag if you are using an uncased model.")
+    parser.add_argument("--per_gpu_train_batch_size", default=8, type=int, help="Batch size per GPU/CPU for training.")
+    parser.add_argument("--per_gpu_eval_batch_size", default=8, type=int, help="Batch size per GPU/CPU for evaluation.")
+    parser.add_argument("--learning_rate", default=5e-5, type=float, help="The initial learning rate for Adam.")
+    parser.add_argument("--weight_decay", default=0.0, type=float, help="Weight deay if we apply some.")
+    parser.add_argument("--adam_epsilon", default=1e-8, type=float, help="Epsilon for Adam optimizer.")
+    parser.add_argument("--max_grad_norm", default=1.0, type=float, help="Max gradient norm.")
+    parser.add_argument("--num_train_epochs", default=3.0, type=float, help="Total number of training epochs to perform.")
+    parser.add_argument("--warmup_steps", default=0, type=int, help="Linear warmup over warmup_steps.")
+    parser.add_argument("--no_cuda", action='store_true', help="Avoid using CUDA when available")
+    parser.add_argument('--overwrite_output_dir', action='store_true', help="Overwrite the content of the output directory")
+    parser.add_argument('--overwrite_cache', action='store_true', help="Overwrite the cached training and evaluation sets")
+    parser.add_argument('--seed', type=int, default=42, help="random seed for initialization")
 
     args = parser.parse_args()
 
